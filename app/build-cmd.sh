f2ba0e95a0bf1c475c0f5024516f858ffdf86a47
Dan Wright <dtwright@gmail.com>
2022-11-18 12:54:11 -0500

    commit WIP

--- app/build-cmd.sh
+++ app/build-cmd.sh
@@ -5,4 +5,4 @@ if [ "$side" = "" ]; then
     echo "no side specified"
     exit
 fi
-west build -b nice_nano_v2 -d build/rollow_${side} -- -DSHIELD="rollow_${side} nice_view"
+west build -p -b nice_nano_v2 -d build/rollow_${side} -c -- -DSHIELD="rollow_${side} nice_view"
